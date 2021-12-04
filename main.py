a499d1ea5f23face521b2797c4b104d5e0f98c63
einwille <einwille@fim.uni-passau.de>
2021-12-04 16:26:52 +0100

    lay down structure to start text segmentation

--- main.py
+++ main.py
@@ -18,22 +18,25 @@ def exec():
         break
 
     # [('/home/andreas/dh/text_to_json/text/Eingestellte_Verfahren', 'Eingestellte_Verfahren'), ...]
-    process_paths_types = []  # (path,type)
+    process_paths_and_types = []  # (path,type)
 
     for type in process_types:
-        process_paths_types.append((os.path.join(path_txt, type), type))
+        process_paths_and_types.append((os.path.join(path_txt, type), type))
         d[type] = []
 
-    for (p, t) in process_paths_types:
+    for (p, t) in process_paths_and_types:
         for path, dir, files in os.walk(p):
             # [('00118', '00118-NSJUSTIZ-BAND3-Teil_1-3_1_a_Prozesse_1934-20190129T163853l__PROCESSED.txt'), ...]
-            txt_id = []  # (id,file)
+            ids_and_filenames = []  # (id,file)
             for file in files:
                 id = file[:5]
                 # TODO try catch
-                txt_id.append((int(id), file))
-            txt_id.sort(key=lambda x: x[0])
-            d[t] = [x[1] for x in txt_id]
+                ids_and_filenames.append((int(id), file))
+            ids_and_filenames.sort(key=lambda x: x[0])
+            d[t] = {}
+
+            for (id, f) in ids_and_filenames:
+                d[t][id] = f  # TODO text file segmenattion
 
     with open(os.path.join(cwd, 'output.json'), 'w') as fp:
         json.dump(d, fp)
