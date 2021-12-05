120d6afd96fa9068f2e339a03c4dfde87de2d678
einwille <einwille@fim.uni-passau.de>
2021-12-05 18:08:02 +0100

    Add dir for invalid docs, finish doc process layout and init text segmentation alg

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import os
 import json
+import shutil
+from pathlib import Path
+
+
+class Error(Exception):
+    '''Base class for other exceptions'''
+    pass
+
+
+class InvalidDocumentName(Error):
+    '''Raised when document name is not valid'''
+    pass
+
+
+def text_segmentation_alg(f: str) -> dict:
+    d = {
+        "file_name": f,
+        "Prozessnummer": "???",
+        "Verfahrensnummer": "???",
+        "Name": "???",
+        "Beruf": "???",
+        "Geburtsdatum": "???",
+        "Urteil": "???",
+        "Anlagen": "???",
+    }
+    return d
 
 
 def exec():
-    '''
-    assumptions:
-    + first 5 chars, given a text file, define the order within a process type subdir 
-    '''
     d = {}
     cwd = os.getcwd()
     path_txt = os.path.join(cwd, "text")
-
+    Path(os.path.join(cwd, "output")).mkdir(parents=True, exist_ok=True)
+    path_invalid_docs = os.path.join(cwd, "output/invalid_documents")
+    try:
+        Path(path_invalid_docs).mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        # rm /output/invalid_documents and create a new dir
+        shutil.rmtree(path_invalid_docs, ignore_errors=True)
+        Path(path_invalid_docs).mkdir(parents=True, exist_ok=True)
     process_types = []
 
     for path, dir, files in os.walk(path_txt):
@@ -28,17 +57,26 @@ def exec():
         for path, dir, files in os.walk(p):
             # [('00118', '00118-NSJUSTIZ-BAND3-Teil_1-3_1_a_Prozesse_1934-20190129T163853l__PROCESSED.txt'), ...]
             ids_and_filenames = []  # (id,file)
+
             for file in files:
-                id = file[:5]
-                # TODO try catch
-                ids_and_filenames.append((int(id), file))
+                try:
+                    id = file[:5]
+
+                    if not (len(id) == 5 and id.isdigit()):
+                        raise InvalidDocumentName
+                    else:
+                        ids_and_filenames.append((int(id), file))
+                except InvalidDocumentName:
+                    source = path+"/"+file
+                    dest = os.path.join(cwd, "output/invalid_documents/"+file)
+                    shutil.copyfile(source, dest)
             ids_and_filenames.sort(key=lambda x: x[0])
             d[t] = {}
 
             for (id, f) in ids_and_filenames:
-                d[t][id] = f  # TODO text file segmenattion
+                d[t][id] = text_segmentation_alg(f)
 
-    with open(os.path.join(cwd, 'output.json'), 'w') as fp:
+    with open(os.path.join(cwd, 'output/output.json'), 'w') as fp:
         json.dump(d, fp)
 
 
