4ba3d2fec15774923614fcd29bcfb5eaf591abc0
einwille <einwille@fim.uni-passau.de>
2022-03-30 18:07:09 +0200

    Add setup, Mv requirements, RM pytest from req

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="special_court_munich",
+    version="1.0.0",
+    description="Special Court Munich Digitization",
+    author="Andreas Einwiller",
+    packages=[],
+)
